971367a748cfe039754760ea71dcf1e465404222
Enrique Gongora <EGTech24@gmail.com>
2020-07-27 09:59:50 -0700

    Blank Xcode project

---
+++ GameOfLife/GameOfLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameOfLife
+//
+//  Created by Enrique Gongora on 7/27/20.
+//  Copyright © 2020 Enrique Gongora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
